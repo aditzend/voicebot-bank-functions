8248857e73c9f5a770781cd756a173e511822856
Alexander Ditzend <alex@saia.ar>
2022-05-01 15:54:47 -0300

    first

--- push.sh
+++ push.sh
@@ -1,5 +1,6 @@
+docker build -t alexanderditzend/voicebot-bank-functions:$2 . && \
+docker push alexanderditzend/voicebot-bank-functions:$2 && \
 git add . && \
 git commit -m $1 && \
 git push origin $2 && \
-docker build -t alexanderditzend/voicebot-bank-functions:$2 . && \
-docker push alexanderditzend/voicebot-bank-functions:$2
+echo "Pushed to GitHub and Dockerhub"
